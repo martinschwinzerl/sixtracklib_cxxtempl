7b1e5fa720a7c6819c7b1a406ce7023d4356d0b7
Martin Schwinzerl <martin.schwinzerl@lefou.at>
2019-09-03 21:33:36 +0200

    selective inclusion of sixtracklib headers; added constexpr predicate to help with overloading of tracking maps

--- cxx/common/track/obj_track_traits.hpp
+++ cxx/common/track/obj_track_traits.hpp
@@ -1,7 +1,10 @@
 #ifndef SIXTRACKLIB_COMMON_TRACK_OBJ_TRACK_TRAITS_CXX_HPP__
 #define SIXTRACKLIB_COMMON_TRACK_OBJ_TRACK_TRAITS_CXX_HPP__
 
-#include "sixtracklib/sixtracklib.hpp"
+#include <type_traits>
+
+#include "sixtracklib/common/definitions.h"
+#include "sixtracklib/common/internal/objects_type_id.h"
 
 namespace sixtrack_cxx
 {
@@ -20,6 +23,21 @@ namespace sixtrack_cxx
         return ( sixtrack_cxx::ObjDataTrackTraits< ObjData >::ObjTypeId() !=
                  SIXTRL_CXX_NAMESPACE::OBJECT_TYPE_INVALID );
     }
+
+    template< class PData, class BeData,
+              SIXTRL_CXX_NAMESPACE::object_type_id_t OBJECT_ID >
+    SIXTRL_FN constexpr bool CanBeTrackedAsParticlesOverObjectId()
+    {
+        return ( ( ObjDataTrackTraits< PData >::ObjTypeId() ==
+                   SIXTRL_CXX_NAMESPACE::OBJECT_TYPE_PARTICLE ) &&
+                 ( ObjDataTrackTraits< BeData >::ObjTypeId() == OBJECT_ID ) );
+    };
+
+    template< class PData, class BeData,
+        SIXTRL_CXX_NAMESPACE::object_type_id_t OBJECT_ID >
+    using track_result_t = std::enable_if<
+        sixtrack_cxx::CanBeTrackedAsParticlesOverObjectId<
+        PData, BeData, OBJECT_ID >(), SIXTRL_CXX_NAMESPACE::track_status_t >;
 }
 
 #endif /* SIXTRACKLIB_COMMON_TRACK_OBJ_TRACK_TRAITS_CXX_HPP__ */
